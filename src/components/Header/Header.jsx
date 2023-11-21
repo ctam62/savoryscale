f0a80f7968b73ff5892987ea371fef59ad230d13
claratam <claramtam@gmail.com>
2023-11-20 21:48:40 -0600

    refactor: separate search component from header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
 import './Header.scss';
-import searchIcon from './../../src/assets/icons/search.svg';
+import { Search } from '../Search/Search';
 
 export const Header = () => {
     return (
         <header>
             <div className='header__logo'>SavoryScale</div>
-            <div className="header__search">
-                <img className='header__search-icon' src={searchIcon} alt='search icon' placeholder='What are you craving?' />
-                <input className='header__search-input' type='text' />
-            </div>
+            <h3 className='header__subheader'>Find a recipe and scale it</h3>
+            <Search />
         </header>
     )
 }
---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,15 @@
+import './Search.scss';
+import searchIcon from '../../assets/icons/search.svg';
+
+export const Search = () => {
+    return (
+        <div className="search">
+            <img className='search-icon' src={searchIcon} alt='search icon' />
+            <input
+                className='search-input'
+                type='text'
+                name='search'
+                placeholder='What are you craving?' />
+        </div>
+    )
+}
