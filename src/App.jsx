4e098359ca8b15bd0e43896e1413b8566423ebd0
claratam <claramtam@gmail.com>
2023-11-20 21:48:03 -0600

    fix: update links to components and pages feat: add new fonts; update color-accent variable; update heading mixing font-family; update body font-family

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Header } from '../components/Header/Header';
-import { HomePage } from './../pages/HomePage/HomePage';
+import { Header } from './components/Header/Header';
+import { HomePage } from './pages/HomePage/HomePage';
 
 function App() {
   return (
