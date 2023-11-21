5e3cc82911e24e6a8e4480279241e8ff81ad546f
claratam <claramtam@gmail.com>
2023-11-21 11:54:16 -0600

    feat: move search and filter components to homepage; update header styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,13 @@
 import './HomePage.scss';
+import { Search } from '../../components/Search/Search';
+import { Filters } from '../../components/Filters/Filters';
 
 export const HomePage = () => {
     return (
-        <h1>Find a recipe and scale it</h1>
-    )
-}
+        <main className='home'>
+            <h3 className='home__subheader'>Find a recipe and scale it</h3>
+            <Search />
+            <Filters />
+        </main>
+    );
+};
