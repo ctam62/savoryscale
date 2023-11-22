8e2c4f9923b43fd287cb14a7cd8b85189887f364
claratam <claramtam@gmail.com>
2023-11-21 19:07:22 -0600

    feat: add like button; store liked list in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,46 @@
 import './App.scss';
+import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header/Header';
 import { HomePage } from './pages/HomePage/HomePage';
+import { RecipePage } from './pages/RecipePage/RecipePage';
 
 function App() {
+  const [recipeList, setRecipeList] = useState([]);
+
+  const handleLikeButton = recipe => {
+    const localStorageListRaw = localStorage.getItem("recipeList") || "[]";
+    const localStorageList = JSON.parse(localStorageListRaw);
+
+    if (!localStorageList.map(entry => entry.id).includes(recipe.id)) {
+      localStorageList.push(recipe);
+      setRecipeList([...localStorageList]);
+      localStorage.setItem("recipeList", JSON.stringify(localStorageList));
+    } else {
+      const filteredList = localStorageList.filter(entry => entry.id !== recipe.id);
+      localStorage.setItem("recipeList", JSON.stringify(filteredList));
+      setRecipeList([...filteredList]);
+    }
+  }
+
+  useEffect(() => {
+    const localStorageListRaw = localStorage.getItem("recipeList");
+    localStorageListRaw && setRecipeList(JSON.parse(localStorageListRaw));
+  }, []);
+
+
   return (
     <>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path='/'
+            element={<HomePage recipeList={recipeList} handleLikeButton={handleLikeButton} />} />
+          <Route path='/recipe/:id' element={<RecipePage />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
-}
+  );
+};
 
 export default App;
