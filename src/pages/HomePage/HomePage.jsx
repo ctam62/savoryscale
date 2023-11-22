632224c70cd5b00ebe891ea43b3e361515cab89b
claratam <claramtam@gmail.com>
2023-11-21 18:54:57 -0600

    feat: add components to homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,36 @@
 import './HomePage.scss';
-import { Search } from '../../components/Search/Search';
+// import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { SearchBar } from '../../components/SearchBar/SearchBar';
 import { Filters } from '../../components/Filters/Filters';
+import { SearchResults } from '../../components/SearchResults/SearchResults';
+import { CardGrid } from '../../components/CardGrid/CardGrid';
 
-export const HomePage = () => {
+export const HomePage = ({ recipeList, handleBookmark }) => {
+
+    const [popularRecipes, setPopularRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchPopularRecipes = async () => {
+            const { data } = await axios.get(`${import.meta.env.VITE_APP_API_URL}/api/recipes/popular`)
+        };
+
+        // fetchPopularRecipes();
+    }, []);
+
     return (
-        <main className='home'>
-            <h3 className='home__subheader'>Find a recipe and scale it</h3>
-            <Search />
+        <main className="home">
+            <h3 className="home__subheader">Find a recipe and scale it</h3>
+            <SearchBar />
             <Filters />
+            <CardGrid
+                results={popularRecipes}
+                title="title"
+                image="image"
+                handleBookmark={handleBookmark}
+                recipeList={recipeList}
+            />
+            <SearchResults recipeList={recipeList} handleBookmark={handleBookmark} />
         </main>
     );
 };
