53bd35efb3344c340c96efaeaecb77ab0ce70d3c
claratam <claramtam@gmail.com>
2023-11-24 14:36:06 -0600

    feat: add recipe scaling calculations based on serving size

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -2,37 +2,47 @@ import './RecipeDetails.scss';
 import parseHTML from 'html-react-parser';
 
 export const RecipeDetails = ({ recipe, servings }) => {
+
+    const scale = (origValue) => {
+        const scaleFactor = parseInt(origValue / recipe.servings);
+        return Math.floor(servings * scaleFactor);
+    }
+
+    const protein = scale(recipe.nutrition.nutrients.find(item => item.name === "Protein").amount);
+    const carbs = scale(recipe.nutrition.nutrients.find(item => item.name === "Carbohydrates").amount);
+    const fat = scale(recipe.nutrition.nutrients.find(item => item.name === "Fat").amount);
+
     return (
         <section className="recipe-details">
             <article className="recipe-details__nutrition">
                 <div className="recipe-details__item">
-                    <p>{parseInt(recipe.nutrition.weightPerServing.amount) * servings} g
+                    <p>{Math.floor(recipe.nutrition.weightPerServing.amount * servings)} g
                     </p>
                     <p className="recipe-details__text">Weight</p>
                 </div>
                 <div className="recipe-details__item">
                     <p className="recipe-details__value">
-                        {parseInt(recipe.nutrition.nutrients.find(item => item.name === "Protein").amount) * servings} g
+                        {protein} g
                     </p>
                     <p className="recipe-details__text">Protein</p>
                 </div>
                 <div className="recipe-details__item">
                     <p className="recipe-details__value">
-                        {parseInt(recipe.nutrition.nutrients.find(item => item.name === "Carbohydrates").amount) * servings} g
+                        {carbs} g
                     </p>
                     <p className="recipe-details__text">Carbs</p>
                 </div>
                 <div className="recipe-details__item">
                     <p className="recipe-details__value">
-                        {parseInt(recipe.nutrition.nutrients.find(item => item.name === "Fat").amount) * servings} g
+                        {fat} g
                     </p>
                     <p className="recipe-details__text">Fat</p>
                 </div>
             </article>
 
             <p className="recipe-details__summary">
-                {parseHTML(recipe.summary)}
+                {parseHTML(recipe.summary.replace(/(\b[^.!?]+spoonacular.*$)/g, ''))}
             </p>
         </section>
-    )
-}
+    );
+};
