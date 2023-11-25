ae99913b6838dcd7ee94b61e5553b059d4f8209f
claratam <claramtam@gmail.com>
2023-11-24 21:12:33 -0600

    feat: update recipe page hero to include estimated cost; add price icon asset

--- src/components/RecipeHero/RecipeHero.jsx
+++ src/components/RecipeHero/RecipeHero.jsx
@@ -1,23 +1,31 @@
 import './RecipeHero.scss';
 import clockIcon from '../../assets/icons/clock.svg';
 import calIcon from '../../assets/icons/fire.svg';
+import moneyIcon from '../../assets/icons/price.svg';
 
-export const RecipeHero = ({ recipe }) => {
+export const RecipeHero = ({ recipe, servings }) => {
+    const cals = Math.floor(recipe.nutrition.nutrients.find(item => item.name === "Calories").amount / recipe.servings * servings);
+    const cost = ((recipe.pricePerServing / 100) * servings).toFixed(2);
+
     return (
         <section className="recipe-hero">
             <img className="recipe-hero__image" src={recipe.image} alt="recipe images" />
 
             <h2 className="recipe-hero__subheader">{recipe.title}</h2>
             <h3>{`${recipe.cuisines.length !== 0 ? (recipe.cuisines.slice(-1) + " Cuisine") : ""}`}</h3>
-            <div className="recipe-hero__stats">
+            <div className="recipe-hero__info">
                 <div className="recipe-hero__time">
-                    <img className="recipe-hero__stats-icons" src={clockIcon} alt="cooking time icon" />
-                    <p className="recipe-hero__stats-text">{recipe.readyInMinutes} min</p>
+                    <img className="recipe-hero__info-icons" src={clockIcon} alt="cooking time icon" />
+                    <p className="recipe-hero__info-text">{recipe.readyInMinutes} min</p>
+                </div>
+                <div className="recipe-hero__price">
+                    <img className="recipe-hero__info-icons" src={moneyIcon} alt="price icon" />
+                    <p className="recipe-hero__info-text">Est. {cost}</p>
                 </div>
                 <div className="recipe-hero__cals">
-                    <img className="recipe-hero__stats-icons" src={calIcon} alt="cals icons" />
-                    <p className="recipe-hero__stats-text">
-                        {parseInt(recipe.nutrition.nutrients.find(item => item.name === "Calories").amount)} kcal
+                    <img className="recipe-hero__info-icons" src={calIcon} alt="cals icons" />
+                    <p className="recipe-hero__info-text">
+                        {cals} kcal
                     </p>
                 </div>
             </div>
