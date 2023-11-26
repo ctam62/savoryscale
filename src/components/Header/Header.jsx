c0dc74e286c2f561f58e570ed80b277a5b27d6c0
claratam <claramtam@gmail.com>
2023-11-25 22:59:51 -0600

    feat: update responsive styles; add collection and shopping list to header nav

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,29 @@
 import './Header.scss';
+import { useNavigate } from 'react-router-dom';
+import shopIcon from '../../assets/icons/shopping-bag.svg';
+import bookIcon from '../../assets/icons/book.svg';
+
 
 export const Header = () => {
+    const navigate = useNavigate();
+
     return (
         <header className="header">
-            <div className="header__logo">SavoryScale</div>
+            <div className="header__logo" onClick={() => navigate('/')}>SavoryScale</div>
+            <nav className="header__nav">
+                <img
+                    className="header__nav-link"
+                    src={shopIcon}
+                    alt="shopping bag"
+                    onClick={() => navigate('/shoppinglist')}
+                />
+                <img
+                    className="header__nav-link"
+                    src={bookIcon}
+                    alt="saved recipe book"
+                    onClick={() => navigate('/collection')}
+                />
+            </nav>
         </header>
     );
 };
