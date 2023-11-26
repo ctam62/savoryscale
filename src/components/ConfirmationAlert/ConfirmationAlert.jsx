8307e4f2a5d2b6bdbc58d6f580e75fa12044e93c
claratam <claramtam@gmail.com>
2023-11-26 10:23:34 -0600

    feat: add view shopping list link to add to shopping list confirmation alert

--- src/components/ConfirmationAlert/ConfirmationAlert.jsx
+++ src/components/ConfirmationAlert/ConfirmationAlert.jsx
@@ -1,11 +1,15 @@
 import './ConfirmationAlert.scss';
+import { useNavigate } from 'react-router-dom';
 import removeIcon from '../../assets/icons/remove.svg';
 
 
 export const ConfirmationAlert = ({ open, handleClose }) => {
+    const navigate = useNavigate();
+
     return (
         <article className={`confirm ${!open ? "confirm--close" : ""}`}>
             <p className="confirm__message">Your ingredients have been sucessfully added to your shopping list</p>
+            <button className="confirm__button" onClick={() => navigate('/shoppinglist')}>View Shopping List</button>
             <img src={removeIcon} alt="close" className="confirm__close" onClick={handleClose} />
         </article>
     );
