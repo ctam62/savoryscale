6a6e775593d122ee0c2df22a8ccf8a998847535d
claratam <claramtam@gmail.com>
2023-12-04 22:37:39 -0600

    feat: update card title styles; add margin top to search results text; remove extra spacing in component

--- src/components/SearchResults/SearchResults.jsx
+++ src/components/SearchResults/SearchResults.jsx
@@ -3,9 +3,6 @@ import { CardPagination } from '../CardPagination/CardPagination';
 
 
 export const SearchResults = ({ results, recipeList, handleLikeButton, handleRemoveButton }) => {
-
-
-
     return (
         <section className="searchResults">
             <h3 className="searchResults__subheader">{results?.length || 0} results</h3>
