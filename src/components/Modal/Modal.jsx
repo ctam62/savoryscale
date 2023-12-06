8d809e704550d90b59a465565da5702d69cf51ed
claratam <claramtam@gmail.com>
2023-12-05 21:59:28 -0600

    feat: create modal component; install universal cookie package

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,15 @@
 import './Modal.scss';
 
-export const Modal = () => {
+export const Modal = ({ open, handleClose, form, validForm, modalTitle, modalMessage }) => {
     return (
-        <div>Modal</div>
+        <ariticle className={`modal__overlay ${open ? "" : "modal--close"}`}>
+            <div className="modal">
+                <p className={`modal__header ${form && !validForm ? "modal--invalid" : "modal--valid"}`}>{modalTitle}</p>
+                <p className="modal__body">{modalMessage}</p>
+                <div className="modal__footer">
+                    <button className={`modal__confirm ${form && !validForm ? "modal--invalid" : "modal--valid"}`} onClick={handleClose}></button>
+                </div>
+            </div>
+        </ariticle>
     );
 };
